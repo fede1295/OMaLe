5b21d356c68a0b2de111fb1736199ce31d5c4dc0
fede1295 <56728020+fede1295@users.noreply.github.com>
2025-05-08 17:08:59 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -9,7 +9,7 @@ PASSWORD = "123Stella"
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
 USR = "fede1295"
-BRANCH = "main"
+BRANCH = "OMaLe"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
